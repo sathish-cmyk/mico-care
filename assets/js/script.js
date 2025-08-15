76df39c14a8029e2fa277c2421ec670a53a13c9a
Moortthi <moortthi.jk@myambergroup.com>
2025-08-15 12:43:38 +0530

    home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 
-document.addEventListener("scroll", function() {
+function toggleHeaderFixed() {
     const header = document.getElementById("mainHeader");
     if (window.scrollY > 100) {
         header.classList.add("fixed");
     } else {
         header.classList.remove("fixed");
     }
-});
+}
+
+// Run on scroll
+document.addEventListener("scroll", toggleHeaderFixed);
+
+// Run on page load (fix refresh issue)
+window.addEventListener("load", toggleHeaderFixed);
+
