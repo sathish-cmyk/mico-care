197608d81c0e135250486ff761ef8e8889d29516
Moortthi <moortthi.jk@myambergroup.com>
2025-08-18 17:48:08 +0530

    innerpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,15 @@ document.addEventListener("scroll", toggleHeaderFixed);
 // Run on page load (fix refresh issue)
 window.addEventListener("load", toggleHeaderFixed);
 
+
+  const videoBox = document.getElementById("videoBox");
+  const video = document.getElementById("myVideo");
+  const playBtn = videoBox.querySelector(".play-btn");
+  const poster = videoBox.querySelector("img");
+
+  playBtn.addEventListener("click", () => {
+    poster.style.display = "none";   // hide poster
+    playBtn.style.display = "none";  // hide play button
+    video.style.display = "block";   // show video
+    video.play();                    // auto-play
+  });
